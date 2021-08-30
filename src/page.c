297300ce842d4f8fbdb7dfd8db944177eb4d5559
garvel <garvel@unist.ac.kr>
2021-08-30 19:57:37 +0900

    gitignore

---
+++ src/page.c
@@ -0,0 +1,5 @@
+
+
+void * get_map_addr(){
+    
+}
--- src/signal.c
+++ src/signal.c
@@ -2,5 +2,7 @@
 #include <stdio.h>
 
 static void sigsegv_handler(int signal, siginfo_t *si, void * unused){
+    si->si_addr
+    
     return;
 }
